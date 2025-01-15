9f7fbbb24c146678623f52ae054ed25e15712c81
Omkar Mastmardi <155365155+omkarm13@users.noreply.github.com>
2025-01-15 18:05:53 +0530

    Update userControllers.js
    fix: some bug

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -44,7 +44,7 @@ export const loginUser=TryCatch(async(req,res)=>{
     const user=await User.findOne({email});
     if(!user){
         return res.status(400).json({
-            message:"No user found",
+            message:"Email or Password Incorrect.",
         });
     }
     const comaparePassword=await bcrypt.compare(password,user.password);
@@ -52,7 +52,7 @@ export const loginUser=TryCatch(async(req,res)=>{
 
     if(!comaparePassword){
         return res.status(400).json({
-            message:"Incorrect Password",
+            message:"Email or Password Incorrect.",
         });
 
     }
