abdecd03624301a70d04d951e2f00262c9866b5b
Pratik Chavan <Pratik>
2025-03-07 12:29:49 +0530

    Normalize line endings to LF

--- backend/controllers/passport.js
+++ backend/controllers/passport.js
@@ -11,8 +11,9 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.NODE_ENV === "production"
-  ? "http://localhost:5000/api/user/auth/google/callback"
-  : "https://proimg.onrender.com/api/user/auth/google/callback",
+      ? "https://proimg.onrender.com/api/user/auth/google/callback"
+      : "http://localhost:5000/api/user/auth/google/callback",
+    
 
     },
     async (accessToken, refreshToken, profile, done) => {
